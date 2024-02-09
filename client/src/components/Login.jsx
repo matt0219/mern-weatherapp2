e346bff164c7f33fd5a41e6c1c58588ca16e2b4c
matt0219 <robotmatt05@gmail.com>
2024-02-09 09:48:13 -0500

    Frontend gets information, waiting for backend

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/Login.css';
 
-
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can handle the form submission logic, such as sending the data to the server
+    console.log('Form submitted:', formData);
+    // You can also reset the form fields after submission
+    setFormData({
+      username: '',
+      email: '',
+      password: '',
+      password2: ''
+    });
+  };
+
   return (
     <div id="login-box">
-      <div className="left">
-        <h1>Sign up</h1>
-        <input type="text" name="username" placeholder="Username" />
-        <input type="text" name="email" placeholder="E-mail" />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="password" name="password2" placeholder="Retype password" />
-        <input type="submit" name="signup_submit" value="Sign me up" />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="left">
+          <h1>Sign up</h1>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password2"
+            placeholder="Retype password"
+            value={formData.password2}
+            onChange={handleChange}
+          />
+          <input type="submit" name="signup_submit" value="Sign me up" />
+        </div>
+      </form>
       <div className="right">
         <span className="loginwith">Sign in with<br />social network</span>
-        <button className="social-signin facebook">Log in with facebook</button>
+        <button className="social-signin facebook">Log in with Facebook</button>
         <button className="social-signin twitter">Log in with Twitter</button>
         <button className="social-signin google">Log in with Google+</button>
       </div>
